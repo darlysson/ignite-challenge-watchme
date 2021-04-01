64698203400beb334a61ed1cce688c55ccb82361
darlyssonalves <dalves@alter-solutions.com>
2021-04-01 21:15:29 +0100

    Challenge resolved! :D

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+interface sidebarProps {
+  onChangeCategory: (id: number) => void;
+  getSelectedGenreId: number;
+  genres: Array<{
+    id: number;
+    title: string;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  }>
+}
+
+export function SideBar({ genres, getSelectedGenreId, onChangeCategory }: sidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onChangeCategory(genre.id)}
+            selected={getSelectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
